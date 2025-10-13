620a56adae8b4ce6c40a48e70e2b988348c1777a
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-10-13 11:42:53 -0500

    feat: add translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { HomeComponent } from "./home/home.component";
 import { FooterComponent } from './footer/footer.component';
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponentComponent, FooterComponent],
+
+  imports: [RouterOutlet, HeaderComponentComponent, FooterComponent, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'portfolio';
+  private translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.addLangs(['es', 'en']);
+    this.translate.setFallbackLang('en');
+    this.translate.use('en');
+  }
 }
