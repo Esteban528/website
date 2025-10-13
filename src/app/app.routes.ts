9a44174d80a2741dc1109d3c8c7331262e49f86f
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-10-13 17:09:21 -0500

    feat: blog post component updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { BlogComponent } from './blog/blog.component';
 import { ViewPostComponent } from './blog/view-post/view-post.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ProjectsComponent } from './home/projects/projects.component';
+import { BlogComponentComponent } from './blog-component/blog-component.component';
 
 export const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'portfolio', component:PortfolioComponent},
   {path:'projects', component:ProjectsComponent},
   {path:'project/:id', component:ProjectViewComponent},
-  {path:'blog', component:BlogComponent},
+  {path:'blog', component:BlogComponentComponent},
   {path:'blog/post/:id', component:ViewPostComponent},
 ];
