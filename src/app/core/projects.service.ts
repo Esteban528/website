a0c434b12816aacf90a0653c6bf04083c39316e7
Esteban Gonzalez <esteban@estebandev.xyz>
2025-11-02 12:12:55 -0500

    fix: incorrect environment import fixed

--- src/app/core/projects.service.ts
+++ src/app/core/projects.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.development';
 import { ApiRequest, ApiService, Project, Resource } from './api.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
