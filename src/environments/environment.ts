7bdd0a20ea4e74fcc781fa2d48374a339dd674fe
Esteban Gonzalez <esteban@estebandev.xyz>
2025-11-06 20:17:35 -0500

    fix: change post url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   projectsUrl : 'https://api.estebandev.xyz/projects',
   resourcesUrl : 'https://api.estebandev.xyz/resources',
-  postsUrl : 'https://api.estebandev.xyz/posts',
+  postsUrl : 'https://api.estebandev.xyz/post',
 };
